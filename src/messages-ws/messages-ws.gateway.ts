f11ff2857f052f52664feebb0b26cc0be83bd91d
Pablo Silva <pmarcelosilvag@gmail.com>
2024-02-09 22:45:27 -0400

    implementacion de websockets

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,19 +1,29 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+
+import { Server,Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
 
 //Los GateWays son como los controladores
 @WebSocketGateway({cors:true})
 export class MessagesWsGateway implements OnGatewayConnection,OnGatewayDisconnect {
+  
+  // WebSocketServer tiene la info de todos los clientes conectados
+  // wss es un nombre que pusimos para el manejo de este decorador
+  @WebSocketServer() wss:Server
   constructor(
     private readonly messagesWsService: MessagesWsService
     ) {}
 
 
   handleConnection(client:Socket, ...args: any[]) {
-    console.log('Cliente conectado',client.id);
+  this.messagesWsService.registerClient(client)
+  this.wss.emit('clients-updated',this.messagesWsService.getConnectedClient())
+
   }
   handleDisconnect(client: Socket) {
-    console.log('Cliente desconectado',client.id);
+   this.messagesWsService.removeClient(client.id)
+   this.wss.emit('clients-updated',this.messagesWsService.getConnectedClient())
   }
+
+  
 }
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+
+
+interface ConnectedClients{
+   //    id   :value
+   [id:string]:Socket 
+}
 
 @Injectable()
-export class MessagesWsService {}
+export class MessagesWsService {
+      // creamos esta propiedad para manejar a todos nuestros clientes conectados
+     private connectedClients:ConnectedClients={}
+     
+     // registrar los clientes conectados
+     registerClient(client:Socket){
+        this.connectedClients[client.id]=client
+        
+     }
+
+     removeClient(clientId:string){
+        delete this.connectedClients[clientId]
+     }
+    // este metodo es para retornar la cantidad de usuarios conectados
+     getConnectedClient():string[]{
+        return  Object.keys(this.connectedClients) 
+     }
+
+
+}
